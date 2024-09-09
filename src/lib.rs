a7f9885f76fc46e62b77941f672336dad04fe92d
David Sánchez <davidslt+git@pm.me>
2024-09-09 23:17:39 +0100

    feat: `no_std` runs successfully

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![no_std]
+
+use libc_print::libc_println;
+
 #[cfg_attr(target_os = "linux", link_section = ".init_array")]
 pub static LD_PRELOAD_INITIALISE_RUST: extern "C" fn() = self::ld_preload_initialise_fn;
 
@@ -6,5 +10,5 @@ extern "C" fn ld_preload_initialise_fn() {
     // panic!("LET'S FAIL!");
 
     // Just printing something
-    println!("HOLA!");
+    libc_println!("HOLA!");
 }
