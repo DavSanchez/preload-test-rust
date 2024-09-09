b34bd46702270a9a274d92083bbd12b74b41cc13
David Sánchez <davidslt+git@pm.me>
2024-09-09 15:23:43 +0100

    chore: cleanup comment

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@ use std::collections::HashMap;
 #[cfg_attr(target_os = "linux", link_section = ".init_array")]
 pub static LD_PRELOAD_INITIALISE_RUST: extern "C" fn() = self::ld_preload_initialise_fn;
 
-// Test this for linux with `LD_PRELOAD=$(pwd)/target/debug/libpreload_rs.so bash -c "echo the env var value is \'\$SOMEVAR\'"`
 extern "C" fn ld_preload_initialise_fn() {
-    // Did some test with a panicking function. See notes in `README.md`.
+    // Did some test with a panicking function.
     // panic!("LET'S FAIL!");
 
     // Just printing something
